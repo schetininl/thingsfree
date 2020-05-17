012e5e898753680aa65f2a0bafeea69479154daf
Oleg Goncharov <oleg.gnch@gmail.com>
2020-05-17 16:50:57 +0300

    THFREE-38. Change structure of project

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    phone_number = PhoneNumberField(
+        _('phone'),
+        unique=True,
+        error_messages={
+            'unique': _('A user with that phone already exists.')
+        }
+    )
+    avatar = models.CharField(
+        _('photo'),
+        max_length=200,
+        blank=True
+    )
+
+    USERNAME_FIELD = 'phone_number'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from phone_verify.api import VerificationViewSet
+from phone_verify.serializers import PhoneSerializer, SMSVerificationSerializer
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import CreateUserSerializer
+
+User = get_user_model()
+
+
+class PhoneVerificationViewSet(VerificationViewSet):
+
+    @action(detail=False, methods=['POST'], permission_classes=[AllowAny],
+            serializer_class=PhoneSerializer)
+    def register(self, request):
+        phone_number = request.data.get('phone_number')
+        if User.objects.filter(phone_number=phone_number).exists():
+            return Response(
+                {'message': _('A user with that phone already exists.')},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().register(request)
+
+    @action(detail=False, methods=['POST'], permission_classes=[AllowAny],
+            serializer_class=CreateUserSerializer)
+    def signup(self, request):
+        serializer = SMSVerificationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        serializer = CreateUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
