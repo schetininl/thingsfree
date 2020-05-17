6d79c4e48cff1157bcdf104a464de28c5d2aeef2
Oleg Goncharov <oleg.gnch@gmail.com>
2020-05-17 17:42:28 +0300

    THFREE-38. Extend user model by city and id; create models for contacts

--- users/models.py
+++ users/models.py
@@ -1,11 +1,16 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
+from cities.models import City
+
 
 class User(AbstractUser):
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = PhoneNumberField(
         _('phone'),
         unique=True,
@@ -18,5 +23,43 @@ class User(AbstractUser):
         max_length=200,
         blank=True
     )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='users'
+    )
 
     USERNAME_FIELD = 'phone_number'
+
+
+class TypeOfContact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(_('name'), max_length=50)
+
+    class Meta:
+        verbose_name = _('Type of contact')
+        verbose_name_plural = _('Types of contact')
+
+    def __str__(self):
+        return self.name
+
+
+class UserContact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='contact_info'
+    )
+    type = models.ForeignKey(
+        TypeOfContact,
+        on_delete=models.CASCADE,
+        related_name='contact_info'
+    )
+    text = models.TextField(_('text'))
+
+    class Meta:
+        verbose_name = _('User contact')
+        verbose_name_plural = _('User contacts')
