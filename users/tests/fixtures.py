30235762ee3c36aac3e0c9cb7ac9c2e7a0b314cb
Oleg Goncharov <oleg.gnch@gmail.com>
2020-06-10 02:29:49 +0300

    THFREE-86. Create test case for invalid phone number

--- users/tests/fixtures.py
+++ users/tests/fixtures.py
@@ -1,5 +1,20 @@
 import pytest
 
+
+@pytest.fixture
+def sms_send_method():
+    """
+    Метод, который выполняет отправку СМС и который будет 'замокан'
+    при тестировании
+    """
+    return 'phone_verify.backends.twilio.TwilioBackend.send_sms'
+
+
 @pytest.fixture
 def valid_phone_number():
     return '+79604566768'
+
+
+@pytest.fixture
+def invalid_phone_number():
+    return 'abc123456'
--- users/tests/test_registration.py
+++ users/tests/test_registration.py
@@ -6,20 +6,22 @@ import pytest
 
 class TestSecurityCodeSending:
 
+    url = '/api/v1/phone/register/'
+
+    @pytest.fixture(autouse=True)
+    def methods_for_mock(self, sms_send_method):
+        self.sms_send_method = sms_send_method
+
     @pytest.mark.django_db(transaction=True)
     def test_successful_sending(self, client, valid_phone_number):
 
-        url = '/api/v1/phone/register/'
-        msg_pattern = f'При POST запросе {url} с валидными данными {{}}'
+        msg_pattern = f'При POST запросе {self.url} с валидными данными {{}}'
 
-        with mock.patch(
-                'phone_verify.backends.twilio.TwilioBackend.send_sms'
-        ) as sender_mock:
-
+        with mock.patch(self.sms_send_method) as sender_mock:
             request_body = {
                 'phone_number': valid_phone_number
             }
-            response = client.post(url, data=request_body)
+            response = client.post(self.url, data=request_body)
 
             assert response.status_code == 200, \
                 msg_pattern.format('HTTP статус ответа не равен 200')
@@ -53,3 +55,32 @@ class TestSecurityCodeSending:
 
             assert verification.security_code in sent_message, \
                 msg_pattern.format('СМС не содержит код подтверждения')
+
+    @pytest.mark.django_db(transaction=True)
+    def test_invalid_phone_number(self, client, invalid_phone_number):
+
+        msg_pattern = f'При POST запросе {self.url} с невалидными данными {{}}'
+
+        with mock.patch(self.sms_send_method) as sender_mock:
+            request_body = {
+                'phone_number': invalid_phone_number
+            }
+            response = client.post(self.url, data=request_body)
+
+            assert response.status_code == 400, \
+                msg_pattern.format('HTTP статус ответа не равен 400')
+
+            response_data = response.json()
+
+            assert response_data.get('status') == 400001, \
+                msg_pattern.format('статус бизнес-логики не равен 400001')
+
+            queryset = SMSVerification.objects.filter(
+                phone_number=invalid_phone_number
+            )
+            assert not queryset.exists(), \
+                msg_pattern.format('сгенерирован код подтверждения')
+
+            response_body = response_data.get('body')
+            assert response_body.get('message') == 'Phone number is not valid.', \
+                msg_pattern.format('ответ содержит неверное сообщение')
