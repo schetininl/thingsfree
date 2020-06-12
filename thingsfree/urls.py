662b6b4ed67598e47f43b58378760a651d8df2f3
Яковлева Светлана <svetlana1905.iakovleva@yandex.ru>
2020-06-13 01:05:38 +0300

    photos

--- thingsfree/urls.py
+++ thingsfree/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/v1/', include('offers.urls')),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('api/v1/', include('users.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
