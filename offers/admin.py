e3c1d9f1e90b4fb9bafa9bdddbaaa2eafadff953
ren.kaio <ren.kaio@yandex.ru>
2020-06-13 15:34:50 +0300

    admin.py corrected v2

--- offers/admin.py
+++ offers/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import OfferCategory
+from .models import Offer, OfferCategory, OfferPhoto
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "title", "description", "category", "is_service",
+     "is_used", "city", "pub_date", "is_private", "moderation_statuses", "is_closed", "close_reason")
+    empty_value_display = "-пусто-"
 
 
 class OfferCategoryAdmin(admin.ModelAdmin):
@@ -8,4 +14,11 @@ class OfferCategoryAdmin(admin.ModelAdmin):
     empty_value_display = "-пусто-"
 
 
+class OfferPhotoAdmin(admin.ModelAdmin):
+    list_display = ("pk", "offer", "link")
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(OfferPhoto, OfferPhotoAdmin)
+admin.site.register(Offer, OfferAdmin)
 admin.site.register(OfferCategory, OfferCategoryAdmin)
