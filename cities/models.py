e63dbf851b46493e63d8b906d13c44c751f3b741
Oleg Goncharov <oleg.gnch@gmail.com>
2020-06-17 01:15:29 +0300

    THFREE-92. Add unique constraints and ordering in models City and Region

--- cities/models.py
+++ cities/models.py
@@ -6,11 +6,12 @@ from django.utils.translation import gettext_lazy as _
 
 class Region(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(_('name'), max_length=50)
+    name = models.CharField(_('name'), max_length=50, unique=True)
 
     class Meta:
         verbose_name = _('Region')
         verbose_name_plural = _('Regions')
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
@@ -28,6 +29,8 @@ class City(models.Model):
     class Meta:
         verbose_name = _('City')
         verbose_name_plural = _('Cities')
+        ordering = ('name',)
+        unique_together = (('name', 'region'),)
 
     def __str__(self):
         return self.name
