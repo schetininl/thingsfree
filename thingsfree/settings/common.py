86ca43de802bcf3235f1769b2ae4b81f31beece0
ren.kaio <ren.kaio@yandex.ru>
2020-08-02 16:44:26 +0300

    page_size modified

--- thingsfree/settings/common.py
+++ thingsfree/settings/common.py
@@ -123,13 +123,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ],
-
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
- 
-    'PAGE_SIZE': 10,
- 
+    ], 
     'EXCEPTION_HANDLER': 'api.views.exception_handler',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+
 }
 
 PHONE_VERIFICATION = {
